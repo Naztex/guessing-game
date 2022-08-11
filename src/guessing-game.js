108bf98d96e1d3c6d02c4b7e8e10d3ea9a0f674d
Naztex <nazteck@yandex.ru>
2022-08-11 12:00:36 +0300

    feat: guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+		this.low = min;
+      this.top = max;  
     }
 
     guess() {
-
+		return this.mid = Math.round((this.low + this.top) / 2);
     }
 
     lower() {
-
+		this.top = this.mid;
     }
 
     greater() {
-
+		this.low = this.mid;
     }
 }
 
